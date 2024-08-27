6070317179288da2a56e4b29726a00360d1a0139
Vamsi-Kommineni <vamsik.kommineni99@gmail.com>
2024-08-27 09:33:26 +0200

    removing unnecessary files from v2.0

--- Code/LLM_loader.py
+++
@@ -1,8 +0,0 @@
-from helper_functions import load_llm
-from configparser import ConfigParser
-
-config = ConfigParser()
-config.read('config.ini')
-model_id = config.get('Models', 'model_id')
-embedding_model_id = config.get('Models', 'embedding_model_id')
-llm = load_llm(model_id, embedding_model_id)
